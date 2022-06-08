742dca728de05d19de56ce65773209117c272b72
max <rodionovmax90test@gmail.com>
2022-06-08 00:17:46 -0400

    init project

---
+++ app/src/main/java/com/example/popularlibraries/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.popularlibraries
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
